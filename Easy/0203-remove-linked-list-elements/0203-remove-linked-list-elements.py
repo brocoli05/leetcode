a9368643645471a49e2585a37415b20df9a4fb27
Minji Kim <105467839+brocoli05@users.noreply.github.com>
2024-02-17 15:11:21 -0500

    Time: 48 ms (67.82%), Space: 19.7 MB (49.57%) - LeetHub

--- Easy/0203-remove-linked-list-elements/0203-remove-linked-list-elements.py
+++ Easy/0203-remove-linked-list-elements/0203-remove-linked-list-elements.py
@@ -5,15 +5,26 @@
 #         self.next = next
 class Solution:
     def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
-        while head is not None and head.val == val:
-            head = head.next
+        ## using recursion
+        if head is None:
+            return
+        
+        head.next = self.removeElements(head.next, val)
+        
+        if head.val == val:
+            return head.next
+        else:
+            return head
+        
+#         while head is not None and head.val == val:
+#             head = head.next
             
-        current = head
+#         current = head
         
-        while current is not None and current.next is not None:
-            if current.next.val == val:
-                current.next = current.next.next
-            else:
-                current = current.next
+#         while current is not None and current.next is not None:
+#             if current.next.val == val:
+#                 current.next = current.next.next
+#             else:
+#                 current = current.next
         
-        return head
+#         return head
