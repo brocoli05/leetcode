08a84f51f0e2b8ded4a592c9acc4557a9e8c623d
Minji Kim <105467839+brocoli05@users.noreply.github.com>
2024-03-27 01:43:16 -0400

    Time: 38 ms (88.86%), Space: 21.9 MB (8.08%) - LeetHub

--- Easy/0125-valid-palindrome/0125-valid-palindrome.py
+++ Easy/0125-valid-palindrome/0125-valid-palindrome.py
@@ -6,17 +6,17 @@ class Solution:
             new_s = [c.lower() for c in s if c.isalnum()]
            
             # Check if the filtered string is a palindrome
-            # left, right = 0, len(new_s) - 1
-            # while left < right:
-            #     if new_s[left] != new_s[right]:
-            #         return False
-            #     left += 1
-            #     right -= 1
-            # return True
-            mid = len(new_s)//2
-            for i in range(mid):
-                j = len(new_s) -i -1
-                if new_s[i] != new_s[j]:
+            left, right = 0, len(new_s) - 1
+            while left < right:
+                if new_s[left] != new_s[right]:
                     return False
-            return True        
+                left += 1
+                right -= 1
+            return True
+            # mid = len(new_s)//2
+            # for i in range(mid):
+            #     j = len(new_s) -i -1
+            #     if new_s[i] != new_s[j]:
+            #         return False
+            # return True        
             
